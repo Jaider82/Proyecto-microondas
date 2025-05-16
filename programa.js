5ebb1b259ed4127c0f30760a60a41f59eb45ec01
jaider beltran <jaiderorlandobeltran@gmail.com>
2025-05-16 15:41:04 -0500

    v.2

---
+++ programa.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([4.627947, -74.065791], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([4.627947, -74.065791]).addTo(map);
